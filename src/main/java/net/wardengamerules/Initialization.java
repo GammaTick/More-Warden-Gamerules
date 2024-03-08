b4308e7e6dd0ab9e6cbe69561a83e6ac1425cec6
JurassicBeast <chilerdnokuk@gmail.com>
2024-03-09 01:43:48 +0200

    Version 0.0.1

---
+++ src/main/java/net/wardengamerules/Initialization.java
@@ -0,0 +1,14 @@
+package net.wardengamerules;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Initialization implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("more-warden-gamerules");
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("More Warden Gamerules were added to your game successfully!");
+	}
+}
