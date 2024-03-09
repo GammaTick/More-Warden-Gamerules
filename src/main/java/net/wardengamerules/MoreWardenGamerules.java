f290c49f4c6bf4902ada5418bffa0aa7d97d35a8
JurassicBeast <chilerdnokuk@gmail.com>
2024-03-09 12:16:32 +0200

    Version 0.0.2 - Added a new gamerule: "CanWardenDisableShields"

--- src/main/java/net/wardengamerules/MoreWardenGamerules.java
+++ src/main/java/net/wardengamerules/MoreWardenGamerules.java
@@ -6,11 +6,14 @@ import net.minecraft.world.GameRules;
 
 public class MoreWardenGamerules {
     public static GameRules.Key<GameRules.BooleanRule> CAN_WARDEN_USE_SONIC_BOOM;
+    public static GameRules.Key<GameRules.BooleanRule> CAN_WARDEN_DISABLE_SHIELD;
+
 
     public MoreWardenGamerules() {
     }
 
     public static void register() {
         CAN_WARDEN_USE_SONIC_BOOM = GameRuleRegistry.register("canWardenUseSonicBoom", GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
+        CAN_WARDEN_DISABLE_SHIELD = GameRuleRegistry.register("canWardenDisableShields", GameRules.Category.MOBS, GameRuleFactory.createBooleanRule(true));
     }
 }
---
+++ src/main/java/net/wardengamerules/mixin/CanWardenDisableShields.java
@@ -0,0 +1,23 @@
+package net.wardengamerules.mixin;
+
+import net.minecraft.entity.mob.WardenEntity;
+import net.wardengamerules.MoreWardenGamerules;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin({WardenEntity.class})
+public abstract class CanWardenDisableShields {
+    public CanWardenDisableShields() {
+    }
+
+    @Inject(
+            method = {"disablesShield"},
+            at = {@At("TAIL")},
+            cancellable = true
+    )
+    protected boolean disablesShield(CallbackInfoReturnable<Boolean> info) {
+        return ((WardenEntity) (Object) this).getWorld().getGameRules().getBoolean(MoreWardenGamerules.CAN_WARDEN_DISABLE_SHIELD);
+    }
+}
